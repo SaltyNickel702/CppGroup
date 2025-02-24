dce146825e6736359e00345465668364bc9d3ef8
Nicholas Wolf <niko.m.wolf@gmail.com>
2025-02-24 12:02:16 -0700

    Update Madlib.h

---
+++ Madlibs/src/Madlib.h
@@ -0,0 +1,15 @@
+#ifndef MADLIB_H
+#define MADLIB_H
+
+#include <iostream>
+using namespace std;
+
+class Madlib {
+    public:
+        Madlib();
+        
+        void addOpperator (string type);
+        void addSentence (string s);
+};
+
+#endif
