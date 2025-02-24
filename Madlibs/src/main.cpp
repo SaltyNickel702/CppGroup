6a561e760aa9ecdd9e1cdcc0ef12fce99efdcf47
Rex <ulissespineda84@gmail.com>
2025-02-24 12:37:23 -0700

    story selector

--- Madlibs/src/main.cpp
+++ Madlibs/src/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <string>
 #include "Madlib.h"
 using namespace std;
+
+int StorySelector(){
+    int story;
+    cout << "Select A story: " << endl;
+    cout << "1,2,3,4,5" << endl;
+    cin >> story;
+    return story;
+}
+
 int main()
 {
-    cout<< "Hello world" << endl;
+    cout<< "Welcome to the Madlibs App." << endl;
+    StorySelector();
     return 0;
 }
