2992ca262805462e774ce58a139561a8efce614e
Nicholas Wolf <niko.m.wolf@gmail.com>
2025-03-12 11:26:23 -0600

    Update chunk.h

--- Minecraft/src/chunk.h
+++ Minecraft/src/chunk.h
@@ -1,6 +1,6 @@
 class Chunk {
     int blocks[16][16][64]; //xyz
 
-    const int getChunkMesh ();
+    const int getChunkMesh (); //replace int with data type
     static int joinChunks (int x, int y, Chunk chunks[]);
 };
