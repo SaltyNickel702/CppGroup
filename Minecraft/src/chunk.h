c39051002492c3dcea163860dfbd5faec30dd96f
Nicholas Wolf <niko.m.wolf@gmail.com>
2025-03-12 12:09:27 -0600

    Added Terrain Gen function defs

--- Minecraft/src/chunk.h
+++ Minecraft/src/chunk.h
@@ -2,16 +2,16 @@
 #define CHUNK_H
 
 class Chunk {
+    static int chunkCoordToWorld (int cx, int cy, float x, float y, float z); //chunk x and y, and relative xyz of coord
+
     int blocks[16][16][64]; //xyz
 
     const int getChunkMesh (); //replace int with data type
     static int joinChunkMeshes (int x, int y, Chunk chunks[]);
-
-    static int chunkCoordToWorld (int cx, int cy, float x, float y, float z); //chunk x and y, and relative xyz of coord
-
     static Chunk genChunk(int cx, int cy);
 
     void setBlock(float x, float y, float z, int blockType);
+    int getBlock(float x, float y, float z);
 };
 
 
