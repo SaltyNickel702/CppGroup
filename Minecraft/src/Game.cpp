924bbcec66e0489baeefc4cd0e34cf85b2fdeb2b
Nicholas Wolf <niko.m.wolf@gmail.com>
2025-03-24 12:03:59 -0600

    Revert "shader"
    This reverts commit 133db9964c8adec385adb47511ea214fe7637ec1.

--- Minecraft/src/Game.cpp
+++ Minecraft/src/Game.cpp
@@ -69,6 +69,61 @@ namespace Game {
 		// glViewport(0,0,w,h);
 
 		//Shader Compilation + triangle test
+
+		//Vertex Shader
+		// const char *vertexShaderSource = "#version 330 core\n"
+		// 	"layout (location = 0) in vec3 aPos;\n"
+		// 	"void main()\n"
+		// 	"{\n"
+		// 	" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		// 	"}\0";
+		// unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		// glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+		// glCompileShader(vertexShader);
+		// // check for shader compile errors
+		// int success;
+		// char infoLog[512];
+		// glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+		// if (!success)
+		// {
+		// 	glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		// 	std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		// }
+
+		// //Fragment Shader
+		// const char* fragmentShaderSource = "#version 330 core\n"
+		// 	"out vec4 FragColor;\n"
+		// 	"void main()\n"
+		// 	"{\n"
+		// 	"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		// 	"}\0";
+		// unsigned int fragmentShader;
+		// fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+		// glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+		// glCompileShader(fragmentShader);
+
+		// glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+		// if(!success)
+		// {
+		// 	glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		// 	cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << endl;
+		// }
+
+		// //Link to Shader Program
+		// unsigned int shaderProgram;
+		// shaderProgram = glCreateProgram();
+		// glAttachShader(shaderProgram,vertexShader);
+		// glAttachShader(shaderProgram,fragmentShader);
+		// glLinkProgram(shaderProgram);
+		// glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+		// if(!success) {
+		// 	glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		// 	cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << endl;
+		// }
+
+		// //delete shader programs
+		// glDeleteShader(vertexShader);
+		// glDeleteShader(fragmentShader);
 		Shader shaderProgram("basicVert.glsl","basicFrag.glsl");
 
 
@@ -102,8 +157,7 @@ namespace Game {
 		glEnableVertexAttribArray(1);
 
 		// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0); 
 
 		// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
 		// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
