821eb814fc12a242cbf117034c9e7b46c040ee68
Nicholas Wolf <niko.m.wolf@gmail.com>
2025-03-24 12:04:27 -0600

    Revert "a"
    This reverts commit f587376e488bdc8b3d98209ffd128cdd0eb40c4a.

--- Minecraft/assets/shaders/basicVert.glsl
+++ Minecraft/assets/shaders/basicVert.glsl
@@ -1,4 +1,4 @@
-#version 330 core
+#version 340 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 colorIn;
 
@@ -9,7 +9,7 @@ out vec4 color;
 
 void main()
 {
-	gl_Position = vec4(aPos.x,aPos.y,aPos.z,1.0);
+	gl_Position = vec4(aPos.x + sin(time*aPos.x)/2, aPos.y-sin(time*aPos.y)/2*(aPos.x+.1), aPos.z, 1.0);
 
 	color = vec4(colorIn.x,colorIn.y,colorIn.z,1.0);
 }
