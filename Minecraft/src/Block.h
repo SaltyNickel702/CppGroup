29c73bc8899a0d866c06bafb289850bc536a135e
Sean G <seangand9606@graniteschools.org>
2025-03-26 12:35:45 -0600

    Added to Block creation

--- Minecraft/src/Block.h
+++ Minecraft/src/Block.h
@@ -8,6 +8,7 @@ using namespace std;
 class Block {
     public:
         Block ();
+        string displayName;
 };
 
 
--- Minecraft/src/main.cpp
+++ Minecraft/src/main.cpp
@@ -1,15 +1,30 @@
 #include "Game.h" //includes all needed includes
 
 void assignBlocks () {
-    Block airBlock();
+    Block airBlock;
+    airBlock.displayName = "Air";
+    airBlock.blockType = 0;
+    World::blockTypes[0] = airBlock;
 
-    Block grassBlock();
+    Block grassBlock;
+    grassBlock.displayName = "Grass";
+    grassBlock.blockType = 1;
+    World::blockTypes[1] = grassBlock;
 
-    Block dirtBlock();
+    Block dirtBlock;
+    dirtBlock.displayName = "Dirt";
+    dirtBlock.blockType = 2;
+    World::blockTypes[2] = dirtBlock;
 
-    Block stoneBlock();
+    Block stoneBlock;
+    stoneBlock.displayName = "Stone";
+    stoneBlock.blockType = 3;
+    World::blockTypes[3] = stoneBlock;
 
-    Block woodBlock();
+    Block woodBlock;
+    woodBlock.displayName = "Wood";
+    woodBlock.blockType = 4;
+    World::blockTypes[4] = woodBlock;
 
 }
 
